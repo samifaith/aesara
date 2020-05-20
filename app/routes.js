089563f12cace12c1e0140477df8faa425323c37
samifaith <60042337+samifaith@users.noreply.github.com>
2020-05-20 18:59:52 -0400

    local-host
    changed from local-host to heroku

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (app){
-    app.get('/', (req, res) => {
-        // console.log(res)
-        console.log(__dirname)
-        // console.log(req)
-        res.render(__dirname + 'index.html');
-      })
-      
-}
